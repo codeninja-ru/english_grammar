9180f17fc41f3423722cd04fb19c03fe38069208
codeninja <vital@ubuntu-home.(none)>
2013-01-06 18:03:20 +1100

    added raws

---
+++ raw/parse_questions.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+
+file_name = ARGV.first
+if !file_name.nil? then
+
+  doc = Nokogiri::HTML(open(file_name), nil, 'UTF-8')
+  doc.css("script").remove
+  doc.css("span").each { |node| node['class'] = 'rules' }
+  doc.css("h2").each do |node|
+    node.content = node.content.strip
+    node.content = node.content.lines.map { |item| item.strip }.join(" ")
+  end
+
+  doc.css("body").first.children { |node| puts node.class }
+
+  IO.write(file_name, doc.to_html)
+end
